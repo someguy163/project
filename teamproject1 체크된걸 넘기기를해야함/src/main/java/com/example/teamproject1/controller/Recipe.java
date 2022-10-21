77f269575800e61486eb6388e45fe77ef0e3bd07
someguy163 <110593873+someguy163@users.noreply.github.com>
2022-10-21 17:11:22 +0900

    Add files via upload

---
+++ teamproject1 체크된걸 넘기기를해야함/src/main/java/com/example/teamproject1/controller/Recipe.java
@@ -0,0 +1,99 @@
+package com.example.teamproject1.controller;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import com.example.teamproject1.dto.DataForm;
+import com.example.teamproject1.entity.Data;
+import com.example.teamproject1.repository.DataRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.swing.*;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
+
+import java.util.List;
+import java.util.SimpleTimeZone;
+
+@Controller
+@Slf4j
+public class Recipe {
+
+    @Autowired
+
+    private DataRepository dataRepository;
+
+    @GetMapping("/home")
+    public String pagehome() {
+        return "view/home";
+    }
+    @PostMapping("/view/create")
+    public String homecreate(DataForm form, Model model){
+        log.info(form.toString());
+
+        //1.dto를 entity로 변환
+        Data data = form.toEntity();
+        log.info(data.toString());
+
+        //2. entity를 repository에 저장.
+        Data saved= dataRepository.save(data);
+        log.info(saved.toString());
+
+        model.addAttribute("data",saved);
+        //repository에 저장된 정보를 배열로 불러온다.
+        List<Data> Datalist= dataRepository.findAll();
+
+        //Datalist를 오름차순 정렬
+        //Datalist를 내림차순 정렬하려면 Datalist.sort(Comparator.comparing(Data::getName).reversed()); 이다.
+        Datalist.sort(Comparator.comparing(Data::getName));
+        Datalist.sort(Comparator.comparing(Data::getEnddate));
+
+
+
+
+        model.addAttribute("datalist",Datalist);
+        return "view/refrigerator";
+
+    }
+        @GetMapping("/Data/{id}/edit")
+        public String show(@PathVariable Long id, Model model){
+        Data Alldata = dataRepository.findById(id).orElse(null);
+        model.addAttribute("Alldata",Alldata);
+        return "/view/edit";
+}
+
+    @GetMapping("/view/refrigerator")
+    public String pagemypage(Model model){
+        List<Data> Datalist= dataRepository.findAll();
+        model.addAttribute("datalist",Datalist);
+        Datalist.sort(Comparator.comparing(Data::getName));
+        Datalist.sort(Comparator.comparing(Data::getEnddate));
+        return "/view/refrigerator";
+        }
+    @GetMapping("/Community")
+    public String pagelist(){
+        return "view/Community";
+    }
+
+    @GetMapping("/Recipe")
+    public String pagenewdelete(DataForm form, Model model){
+
+        
+
+
+        List<Data> Datalist= dataRepository.findAll();
+        model.addAttribute("datalist",Datalist);
+        Datalist.sort(Comparator.comparing(Data::getName));
+        Datalist.sort(Comparator.comparing(Data::getEnddate));
+
+        return "view/Recipe";
+    }
+
+}
---
+++ teamproject1 체크된걸 넘기기를해야함/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image) values (1,'사과','2022-10-10','2022-11-31','과일','식자재에 대한 정보 입력','사과는 빨간색이다.','css.png');
+insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image) values (2,'딸기','2022-10-10','2022-11-31','과일','식자재에 대한 정보 입력','딸기는 빨간색이다.','flower.png');
+insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image) values (3,'배','2022-10-10','2022-11-31','채소','식자재에 대한 정보 입력','배는 노란색이다.','html-5.png');
