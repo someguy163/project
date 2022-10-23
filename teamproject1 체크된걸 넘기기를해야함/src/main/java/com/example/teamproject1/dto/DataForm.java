579b0e0ff56695dcd073291ad46f7433fed00c1f
someguy163 <110593873+someguy163@users.noreply.github.com>
2022-10-23 18:01:25 +0900

    Add files via upload

---
+++ teamproject1 체크된걸 넘기기를해야함/src/main/java/com/example/teamproject1/dto/DataForm.java
@@ -0,0 +1,59 @@
+package com.example.teamproject1.dto;
+
+import com.example.teamproject1.entity.Data;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
+public class DataForm {
+    private Long id;
+    private String name;
+    private String start;
+
+    private String enddate;
+
+    private String kind;
+
+    private int otherdate;
+
+
+
+    private String nameinfo;
+
+    private String image;
+
+    private String othermonth;
+
+
+
+
+
+
+
+
+
+
+
+  //  public dataForm(String name, String date) {
+    //    this.name = name;
+     //   this.date = date;
+ //   }
+
+  //  @Override
+   // public String toString() {
+    //    return "dateForm{" +
+   //             "name='" + name + '\'' +
+    //            ", date='" + date + '\'' +
+     //           '}';
+
+   // }
+    public Data toEntity(){
+
+        return new Data(id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth);
+    }
+}
