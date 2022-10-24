6fef9cb39ad84480398564bd3e5920aca3ef12df
someguy163 <110593873+someguy163@users.noreply.github.com>
2022-10-24 16:32:44 +0900

    Delete teamproject1 체크된걸 넘기기를해야함 directory

--- teamproject1 체크된걸 넘기기를해야함/out/production/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth) values (1,'사과','2022-07-10','2022-10-10','과일','0','사과는 빨간색이다.','css.png','3');
-insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth) values (2,'딸기','2022-07-10','2022-10-10','과일','0','딸기는 빨간색이다.','flower.png','3');
-insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth) values (3,'배','2022-07-10','2022-10-10','채소','0','배는 노란색이다.','html-5.png','3');
