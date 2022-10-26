970383473c43e203d29a12764e591870b1553938
someguy163 <110593873+someguy163@users.noreply.github.com>
2022-10-27 01:02:47 +0900

    Add files via upload

---
+++ teamproject1 등록css완료/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth,plusdate) values (1,'당근','2022-10-10','2022-11-10','야채','0','2개 구매','carrot.jpg','1','2022-10-10');
+insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth,plusdate) values (2,'토마토','2022-10-10','2022-12-10','야채','0','토마토달걀볶음(3개)','tomato.jpg','2','2022-10-10');
+insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth,plusdate) values (3,'양파','2022-10-10','2022-11-10','야채','0','3개 구매','onion.jpg','1','2022-10-10');
+insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth,plusdate) values (4,'감자','2022-10-10','2022-11-10','야채','0','5개 구매','potato.jpg','1','2022-10-10');
+insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth,plusdate) values (5,'오렌지','2022-10-10','2022-11-10','과일','0','갈아먹을것(4개)','orange.jpg','1','2022-10-10');
+insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth,plusdate) values (6,'사과','2022-10-10','2022-11-10','과일','0','아침에먹을예정(5개)','apple.jpg','1','2022-10-10');
