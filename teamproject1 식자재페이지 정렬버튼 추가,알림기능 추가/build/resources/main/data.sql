805543ff573dce8bed5753044aa0bad67fc53e0a
someguy163 <110593873+someguy163@users.noreply.github.com>
2022-10-30 16:58:31 +0900

    Delete teamproject1 식자재페이지 정렬버튼 추가,알림기능 추가 directory

--- teamproject1 식자재페이지 정렬버튼 추가,알림기능 추가/build/resources/main/data.sql
+++
@@ -1,6 +0,0 @@
-insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth,plusdate) values (1,'당근','2022-10-11','2022-12-10','야채','0','2개 구매','carrot.jpg','1','2022-10-10');
-insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth,plusdate) values (2,'토마토','2022-10-12','2022-11-01','야채','0','토마토달걀볶음(3개)','tomato.jpg','2','2022-10-10');
-insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth,plusdate) values (3,'양파','2022-10-13','2022-11-05','야채','0','3개 구매','onion.jpg','1','2022-10-10');
-insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth,plusdate) values (4,'감자','2022-10-14','2022-12-08','야채','0','5개 구매','potato.jpg','1','2022-10-10');
-insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth,plusdate) values (5,'오렌지','2022-10-15','2022-11-11','과일','0','갈아먹을것(4개)','orange.jpg','1','2022-10-10');
-insert into data (id,name,start,enddate,kind,otherdate,nameinfo,image,othermonth,plusdate) values (6,'사과','2022-10-16','2022-12-25','과일','0','아침에먹을예정(5개)','apple.jpg','1','2022-10-10');
